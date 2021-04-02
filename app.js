9c54f429f704dc71ed4f237299688dd1a1881076
Roaringmind22 <nandor1kovacs@gmail.com>
2021-04-02 14:46:24 +0200

    even better triangle

--- app.js
+++ app.js
@@ -48,7 +48,7 @@ class Triangle {
   constructor(size, origin, rotation) {
     this.a = origin;
     this.b = new Coordinate(Math.cos(rotation * (Math.PI / 180)) * size + origin.x, Math.sin(rotation * (Math.PI / 180)) * size + origin.y);
-    this.c = new Coordinate(Math.cos((rotation + 60) * (Math.PI / 180)) * size + origin.x, (Math.sin(rotation + 60 * (Math.PI / 180)) * size + origin.y) * -1);
+    this.c = new Coordinate(Math.cos((rotation + 60) * (Math.PI / 180)) * size + origin.x, Math.sin(rotation + 60 * (Math.PI / 180)) * size * -1 + origin.y);
   }
 }
 
